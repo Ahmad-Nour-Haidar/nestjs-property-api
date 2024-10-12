6173d3ef0f10be0704bc062a4097ed6d37fa3811
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-10-12 23:29:42 +0300

    add validation headers.dto.ts

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,13 +1,34 @@
-import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Patch,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import {
   createPropertySchema,
   CreatePropertyZodDto,
 } from './dto/create-property-zod.dto';
 import { ZodValidationPipe } from './pipes/zod-validation-pipe';
+import { RequestHeader } from './pipes/request-header-pipe';
+import { HeadersDto } from './dto/headers.dto';
 
 @Controller('property')
 export class PropertyController {
   @Post()
   @UsePipes(new ZodValidationPipe(createPropertySchema))
-  create(@Body() body: CreatePropertyZodDto) {}
+  create(@Body() body: CreatePropertyZodDto) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(
+    @RequestHeader(
+      new ValidationPipe({ validateCustomDecorators: true, whitelist: true }),
+    )
+    header: HeadersDto,
+  ) {
+    return header;
+  }
 }
