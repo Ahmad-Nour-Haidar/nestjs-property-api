04d2bef7de65f3080fa4927468a3f9219d516274
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-10-12 23:03:40 +0300

    add validation with Zod

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+import {
+  createPropertySchema,
+  CreatePropertyZodDto,
+} from './dto/create-property-zod.dto';
+import { ZodValidationPipe } from './pipes/zod-validation-pipe';
 
 @Controller('property')
-export class PropertyController {}
+export class PropertyController {
+  @Post()
+  @UsePipes(new ZodValidationPipe(createPropertySchema))
+  create(@Body() body: CreatePropertyZodDto) {}
+}
