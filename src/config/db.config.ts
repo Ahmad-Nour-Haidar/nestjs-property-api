e2d75d69db76a234836541b6168b7a04316b119b
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-10-13 10:08:05 +0300

    edit db.config.ts

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -3,7 +3,7 @@ import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConne
 export const pgConfig: PostgresConnectionOptions = {
   url: 'postgresql://postgres:123@localhost:5432/nestjs-property-api?schema=public',
   type: 'postgres',
-  port: 3306,
-  entities: [],
+  port: 5432,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: true,
 };
