9af4dac3f5e162b0cbf21a33e2f172c58e39554a
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-10-13 22:57:03 +0300

    add property-feature.entity.ts

---
+++ src/entities/property-feature.entity.ts
@@ -0,0 +1,39 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Property } from './property.entity';
+
+@Entity()
+export class PropertyFeature {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  bedrooms: number;
+
+  @Column()
+  bathrooms: number;
+
+  @Column()
+  parking_spots: number;
+
+  @Column()
+  area: number;
+
+  @Column()
+  has_balcony: boolean;
+
+  @Column()
+  has_garden_yard: boolean;
+
+  @Column()
+  has_swimming_pool: boolean;
+
+  @OneToOne(() => Property, (property) => property.property_feature)
+  @JoinColumn()
+  property: Property;
+}
