be0a029018f509e8ca9daf1afb09f07295de3844
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-10-13 11:47:28 +0300

    fis path to entities

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,9 +1,10 @@
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import * as path from 'node:path';
 
 export const pgConfig: PostgresConnectionOptions = {
   url: 'postgresql://postgres:123@localhost:5432/nestjs-property-api?schema=public',
   type: 'postgres',
   port: 5432,
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  entities: [path.resolve(__dirname, '..') + '/**/*.entity{.ts,.js}'],
   synchronize: true,
 };
