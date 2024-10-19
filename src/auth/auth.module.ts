6438fed7f932a8b262104b7cfda98f452d821440
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-10-19 23:33:35 +0300

    A Trick For Enabling Global Auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,6 +9,9 @@ import { ConfigModule } from '@nestjs/config';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import refreshJwtConfig from './config/refresh-jwt.config';
 import { RefreshJwtStrategy } from './strategies/refresh.strategy';
+import { APP_GUARD } from "@nestjs/core";
+import { JwtAuthGuard } from "./guards/jwt-auth/jwt-auth.guard";
+import { RolesGuard } from "./guards/roles/roles.guard";
 
 @Module({
   imports: [
@@ -18,6 +21,19 @@ import { RefreshJwtStrategy } from './strategies/refresh.strategy';
     ConfigModule.forFeature(refreshJwtConfig),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, JwtStrategy, RefreshJwtStrategy],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+    RefreshJwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard, //@UseGuards(JwtAuthGuard) applied on all API endppints
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
 })
 export class AuthModule {}
